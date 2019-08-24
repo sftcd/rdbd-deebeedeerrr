1c21dc68324bdc01fdd658d6bb281f17e30df5fe
Stephen Farrell <stephen.farrell@cs.tcd.ie>
2019-08-24 16:05:07 +0100

    code now makes sigs; next up output pres fmt

--- ed25519-signer.py
+++ ed25519-signer.py
@@ -1,73 +1,54 @@
 #!/usr/bin/env python3
-# CODE_BEGINS
 import os,argparse, sys, binascii
 from eddsa2 import Ed25519
 from keytag3 import calc_keyid
 
 def main():
     parser=argparse.ArgumentParser(description='Ed25519 signing')
-    parser.add_argument('-s','--secret',dest='secret', help='secret key')
+    parser.add_argument('-s','--secret',dest='secret', help='secret key file')
+    parser.add_argument('-m','--message',dest='tbs', help='to-be-signed data')
     parser.add_argument('-r','--relating',dest='relating', help='relating domain')
     parser.add_argument('-d','--related',dest='related', help='related domain')
-    parser.add_argument('-n','--negative',dest='negative', help='negative assertion')
+    parser.add_argument('-o','--outfile',dest='outfile', help='output file for signature')
+    parser.add_argument('-t','--tag',dest='tag', help='tag value to use')
     args=parser.parse_args()
 
     if args.secret is None:
         print("You do need a secret... - exiting")
         sys.exit(1)
-    secval=args.secret
-    # but... secret can also be a file name, if so, we'll try read that
-    if os.path.isfile(args.secret) and os.access(args.secret,os.R_OK) and os.path.getsize(args.secret)==32:
-        f=open(args.secret,'rb')
-        binval=f.read()
-        f.close()
-        secval=binval
-        secret=secval
-    else:
-        secret=secval.encode('utf-8')
-    # secret has to be 32 octets funnily enuugh:-)
-    # e.g. secret="rdbd-example0001rdbd-example0002".encode('utf-8')
-    if len(secval)!=32:
-        print("Secret has to be 32 octets...(it's " + str(len(secval)) + " - exiting")
-        sys.exit(1)
-    if args.relating is None:
-        print("You do need a relating domain... - exiting")
-        sys.exit(1)
-    if args.related is None:
-        print("You do need a related domain... - exiting")
-        sys.exit(1)
+
+    with open(args.secret,"rb") as f:
+        secret=f.read()
     privkey,pubkey = Ed25519.keygen(secret)
-    print("private:"+ str(binascii.hexlify(privkey)))
-    print("public:"+ str(binascii.hexlify(pubkey)))
 
-    b64pubkey=binascii.b2a_base64(pubkey).rstrip().decode("utf-8")
-    print("b64pubkey: " + b64pubkey)
+    if args.tbs is not None:
+        tbs=args.tbs
+    else:
+        if args.relating is None:
+            print("You do need a relating domain... - exiting")
+            sys.exit(1)
+        if args.related is None:
+            print("You do need a related domain... - exiting")
+            sys.exit(1)
+        rdbdtag="1"
+        if args.tag:
+            rdbdtag=args.tag
 
-    keyid=calc_keyid("0","3","15",b64pubkey)
-    print("keyid: " + str(keyid))
+        b64pubkey=binascii.b2a_base64(pubkey).rstrip().decode("utf-8")
+        keyid=calc_keyid("0","3","15",b64pubkey)
 
-    rdbdtag="1"
-    if args.negative:
-        rdbdtag="0"
-
-    tbs="relating="+args.relating+"\nrelated="+args.related+"\nrdbd-tag="+rdbdtag+"\nkey-tag="+str(keyid)+"\nsig-alg=15\n"
-    print("to-be-signed:|" + str(tbs)+"|")
-
-    with open("ed25519.priv", "wb") as privf:
-        privf.write(privkey)
-    with open("ed25519.pub","wb") as pubf:
-        pubf.write(pubkey)
-    with open("to-be-signed-15.txt","wb") as tbsf:
-        tbsf.write(tbs.encode('utf-8'))
+        tbs="relating="+args.relating+"\nrelated="+args.related+"\nrdbd-tag="+rdbdtag+"\nkey-tag="+str(keyid)+"\nsig-alg=15\n"
+        #print("to-be-signed:|" + str(tbs)+"|")
 
     msg=tbs.encode('utf-8')
     signature = Ed25519.sign(privkey, pubkey, msg)
-    print("sig:"+ str(binascii.hexlify(signature)))
-    with open("ed25519.sig", "wb") as sigf:
-        sigf.write(signature)
+    if args.outfile is not None:
+        with open(args.outfile, "wb") as sigf:
+           sigf.write(signature)
+    else:
+        print(str(binascii.hexlify(signature)))
     return
 
 if __name__ == "__main__":
     main()
 
-# CODE_ENDS
--- make-zonefrags.sh
+++ make-zonefrags.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -x
+# set -x
 
 # Call our zonefile fragment producer code for each
 # key and relevant relationship (or disavowal)
@@ -47,7 +47,6 @@ function hashname()
 : "${EDDSA:="yes"}"
 : "${RSA:="no"}"
 : "${OBIN=`which openssl`}"
-: "${PASS="arse"}"
 
 # set blank values
 RELATING=""
@@ -122,32 +121,67 @@ then
     then
         mkdir -p $PRIVKEYDIR
     fi
-
-    if [ ! -f $privfilename ]
+    if [[ "$EDDSA" == "yes" ]]
     then
-        echo "Can't read $privfilename (based on $RELATING) - making one"
-        if [[ "$EDDSA" == "yes" ]]
+        if [ ! -f $privfilename ]
         then
+            echo "Can't read private key file for $RELATING - making one"
             # 32 random octets are good enough
-            dd if=/dev/urandom count=32 bs=1 >$privfilename
-            $RDIR/ed25519-signer.py -s $privfilename -r $RELATING -d $RELATED >signer.out
-            PUB=`base64 ed25519.pub`
-            KEYID=`$RDIR/keytag3.py -a 15 -p $PUB`
-            sigparms="-s eddsa -p $privfilename"
+            dd if=/dev/urandom count=32 bs=1 >$privfilename 2>/dev/null
+            if [ ! -f $privfilename ]
+            then
+                echo "Could't make private key file for $RELATING - exiting"
+                exit 3
+            fi
+            fsz=`wc -c $privfilename | awk '{print $1}'`
+            if [[ "$fsz" != "32" ]]
+            then
+                echo "Private key file for $RELATING is weird size - exiting"
+                echo "Offending file is $privfilename"
+                exit 4
+            fi
+            $RDIR/ed25519-makekey.py -s $privfilename -p $pubfilename 
         fi
-        if [[ "$RSA" == "yes" ]]
+        if [ ! -f $pubfilename ]
         then
-            $OBIN genrsa -out $privfilename 2048 >/dev/null 2>&1
-            $OBIN rsa -in $privfilename -out $pubfilename.pem -pubout -outform PEM >/dev/null 2>&1
-            PUB=`cat $pubfilename.pem | awk '!/----/' | tr '\n' ' ' | sed -e 's/ //g'`
-            echo -e $PUB >$pubfilename
-            KEYID=`$RDIR/keytag3.py -a 8 -p $PUB`
-            echo "Keyid is $KEYID"
-            sigparms="-s rsa --private $privfilename --public $pubfilename"
+            echo "Something went wrong generating a key pair - exiting"
+            exit 5
         fi
+        PUB=`base64 $pubfilename`
+        KEYID=`$RDIR/keytag3.py -a 15 -p $PUB`
+        sigparms="-s eddsa -p $privfilename --public $pubfilename"
+        tbs="relating=$RELATING\nrelated=$RELATED\nrdbd-tag=$TAG\nkey-tag=$KEYID\nsig-alg=15\n"
+        TMPSIG=`mktemp`
+        $RDIR/ed25519-signer.py -s $privfilename -m $tbs -o $TMPSIG
+        b64sig=`base64 -w 48 $TMPSIG  | sed -e 's/^/            /'`
+        echo "$RELATING->$RELATED sig: $b64sig"
+        rm -f $TMPSIG
+    elif [[ "$RSA" == "yes" ]]
+    then
+        if [ ! -f $privfilename ]
+        then
+            echo "Can't read private key file for $RELATING - making one"
+            $OBIN genrsa -out $privfilename 2048 >/dev/null 2>&1
+            $OBIN rsa -in $privfilename -out $pubfilename -pubout -outform PEM >/dev/null 2>&1
+        fi
+        if [ ! -f $pubfilename ]
+        then
+            echo "Something went wrong generating a key pair - exiting"
+            exit 5
+        fi
+        PUB=`cat $pubfilename | awk '!/----/' | tr '\n' ' ' | sed -e 's/ //g'`
+        KEYID=`$RDIR/keytag3.py -a 8 -p $PUB`
+        sigparms="-s rsa --private $privfilename --public $pubfilename"
+        tbs="relating=$RELATING\nrelated=$RELATED\nrdbd-tag=$TAG\nkey-tag=$KEYID\nsig-alg=8\n"
+        TMPTBS=`mktemp`
+        echo -e $tbs >$TMPTBS
+        TMPSIG=`mktemp`
+        $OBIN dgst -sha256 -sign $privfilename -out $TMPSIG $TMPTBS
+        rm -f $TMPTBS
+        b64sig=`base64 -w 48 $TMPSIG  | sed -e 's/^/            /'`
+        echo "$RELATING->$RELATED sig: $b64sig"
+        rm -f $TMPSIG
     fi
 fi
 
-# call our python zone file fragment producing code
-$RDIR/make-rdbdfrag.py -i $RELATING -d $RELATED $sigparms -t $TTL -o $ZfDIR -t $TAG
 
